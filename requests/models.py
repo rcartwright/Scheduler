80ac39f69ce015909f090f5a26ba483a6007530d
Rachel Cartwright <rachel@scottsmarketplace.com>
2015-08-12 14:43:55 -0700

    add models

--- requests/models.py
+++ requests/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+
+class Request(models.Model):
+    user = models.ForeignKey(User)
+    request_text = models.CharField(max_length=200)
+    request_date = models.DateTimeField('date requested')
