81ec7e10cffcc3303255286d30f9c04e53cd9304
Rachel Cartwright <rachel@scottsmarketplace.com>
2015-08-13 10:24:50 -0700

    add CRUD urls and views

--- requests/urls.py
+++ requests/urls.py
@@ -1,15 +1,14 @@
-from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls import patterns, url
 
 from requests import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
-)
-
-urlpatterns = patterns('',
-    url(r'^requests/', include('requests.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+	# ex: /requests/
+    url(r'^$', views.index, name='index'),
+    # ex: /requests/5/
+    url(r'^(?P<request_id>\d+)/$', views.show, name='show'),
+    # ex: /requests/5/edit/
+    url(r'^(?P<request_id>\d+)/edit/$', views.edit, name='edit'),
+    # ex: /requests/5/delete/
+    url(r'^(?P<request_id>\d+)/delete/$', views.delete, name='delete'),
 )
--- requests/views.py
+++ requests/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Index Page")
+    return HttpResponse("Request Index")
+
+def edit(request, request_id):
+    return HttpResponse("Edit Request" % request_id)
+
+def show(request, request_id):
+    return HttpResponse("Show Request" % request_id)
+
+def delete(request, request_id):
+    return HttpResponse("Delete Request" % request_id)
