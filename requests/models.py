c97748fffef3cd60f267547e1023fdbdd2e0b677
Rachel Cartwright <rachel@scottsmarketplace.com>
2015-08-14 10:26:21 -0700

    add more columns to models

--- requests/models.py
+++ requests/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    admin = models.BooleanField(default=False)
 
     def __str__(self):              # __unicode__ on Python 2
         return "%s" % (self.id)
@@ -13,6 +14,7 @@ class Request(models.Model):
     request_text = models.CharField(max_length=200)
     request_date = models.DateTimeField('date requested')
     days = models.IntegerField()
+    approved = models.BooleanField(default=False)
 
     def __str__(self):              # __unicode__ on Python 2
         return self.request_text
